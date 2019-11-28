36f6350f6c9f4eaa1b7780f733465720793cc3df
Alberto González Balderas <albertoglezb@hotmail.com>
2019-11-28 04:52:10 -0600

    Se inicializó el proyecto y se creó la primer pagina. Se terminó la clase: 'Creando nuestra primera página'.

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+export default class extends React.Component {
+    render() {
+        return <div>
+            <h2>Hola a todos.</h2>
+            <p>Bienvenidos a todos 
+                al curso de Next.js
+            </p>
+        </div>
+
+    }
+}
