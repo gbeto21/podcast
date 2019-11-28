292bf0398afae8e74aa411bb92fd02393f18dec0
Alberto González Balderas <albertoglezb@hotmail.com>
2019-11-28 10:21:57 -0600

    Se modificó la pagina de index para obtener la información de los podcast y se le dió estilo a la página. Se terminó la clase: 'Intro a getInitialProps'.

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,75 @@
+import 'isomorphic-fetch'
+
 export default class extends React.Component {
+
+    static async getInitialProps() {
+        let req = await fetch('https://api.audioboom.com/channels/recommended')
+        let { body: channels } = await req.json()
+        return { channels }
+    }
+
     render() {
+
+        const { channels } = this.props
+
         return <div>
-            <h2>Hola a todos.</h2>
-            <p>Bienvenidos a todos
-                al curso de Next.js
-            </p>
-            <img src='/static/platzi-logo.png' alt="Platzi" />
+
+            <header>Podcasts</header>
+            <div className="channels">
+
+
+                {channels.map((channel) => (
+                    <div className="channel">
+                        <img src={channel.urls.logo_image.original} alt="" />
+                        <h2>{channel.title}</h2>
+                    </div>
+
+                ))}
+
+            </div>
+
             <style jsx>{`
-            h2{color:red;}
-            div :global(p){color: green;}
-            img {
-                max-width: 50%;
-                display: block;
-                margin: 0 auto;
-            }
-            `}</style>
+
+                    header{
+                        color:#fff;
+                        background: #8756ca;
+                        padding: 15px;
+                        text-align:center;
+                    }
+
+                    .channels{
+                        display: grid;
+                        grid-gap: 15px;
+                        padding: 15px;
+                        grid-template-columns: repeat(auto-fill,minmax(160px, 1fr));
+                    }
+
+                    .channel{
+                        display: block;
+                        border-radius: 3px;
+                        box-shadow: 0px 2px 6px rgba(0,0,0,0.15);
+                        margin-bottom: 0.5em;
+                    }
+                    .channel img{
+                        width: 100%;
+                    }
+                    h2{
+                        padding: 5px;
+                        font-size: 0.9em;
+                        font-weight: 600;
+                        margin: 0;
+                        text-align: center;
+                    }
+                    `}
+            </style>
+
             <style jsx global>
                 {`
-                    body {background: yellow;}
+                    body {
+                        margin: 0;
+                        font-family:system-ui;
+                        background: white;
+                    }
     
                     `  }
             </style>
